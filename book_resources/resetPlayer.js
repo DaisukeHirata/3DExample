b7b08349a72f977089bc11af9cff52c7dede4039
Daisuke Hirata <daisukihirata@gmail.com>
2017-09-06 21:59:13 +0900

    add resources

---
+++ book_resources/resetPlayer.js
@@ -0,0 +1,10 @@
+﻿var startPosition:Vector3;
+function Start () 
+{
+	startPosition = transform.position;
+}
+
+function Update () 
+{
+	if (transform.position.y < -5) transform.position = startPosition;
+}
